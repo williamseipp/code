b2189c7039bdb9a6a75959a0d22451172670c273
William <william.seipp@gmail.com>
2025-09-09 23:17:55 -0700

    start todos exercise

---
+++ js230_todos/app.js
@@ -0,0 +1,40 @@
+let todoItems = [
+  {id: 1, title: 'Homework'},
+  {id: 2, title: 'Shopping'},
+  {id: 3, title: 'Calling Mom'},
+  {id: 4, title: 'Coffee with Elphaba'},
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+//   Create a list of todos in the DOM from a JavaScript collection (array) of todos.
+//
+//   Each Todo has an id, name, and a red x
+
+  // each todo is implemented as a list item:
+  // <ul class='todos'>
+  // <li data-id="1">
+  // Homework 
+  // <span class="remove"></span>
+  //
+//   Add a delete button for each todo.
+//
+//   When the user clicks the delete button, a comfirmation prompt appears asking the user to confirm the deletion.
+//
+//
+//   Use CSS to display the comfirmation prompt.
+//
+//   The comfirmation prompt has "Yes" and "No" buttons.
+//
+//   The prompt should include the title of the todo being deleted.
+//
+//   When the user clicks the "Yes" button, the comfirmation prompt disappears, and the corresponding todo is deleted.
+//
+//   When the user clicks the "No" button, the comfirmation prompt disappears, and the todo is not deleted.
+//
+//   When the user clicks outside of the comfirmation prompt, the comfirmation prompt disappears, and the todo is not deleted.
+//
+//
+//
+// Prevent the user from interacting with other buttons when the confirmation prompt is open.
+
+})
