f3e57bef190bc30779cd4b2c57088aeab7df53f8
William <william.seipp@gmail.com>
2025-09-18 11:45:25 -0700

    refactor: extract logic of prompt creation outside of event listener into separate methods

--- js230_todos/app.js
+++ js230_todos/app.js
@@ -17,31 +17,8 @@ class App {
     this.todosDiv.addEventListener('click', event => {
       if(event.target.classList.contains('remove')) {
         let todoId = Number(event.target.closest('li').dataset.id);
-        let title = document.createElement('p');
         let todo = todoItems.find(todo => todo.id === todoId);
-        title.textContent = todo.title;
-
-        let text = document.createElement('p');
-        text.textContent = `Are you sure you want to delete "${title.textContent}"?`
-
-        let yes = document.createElement('a');
-        yes.classList.add('confirm_yes');
-        yes.textContent = 'Yes';
-        yes.setAttribute('href', '#');
-
-        let no = document.createElement('a');
-        no.classList.add('confirm_no');
-        no.textContent = 'No';
-        no.setAttribute('href', '#');
-
-        this.promptDiv.replaceChildren();
-        this.promptDiv.appendChild(text);
-        this.promptDiv.appendChild(yes);
-        this.promptDiv.appendChild(no);
-        this.promptDiv.dataset.id = todoId;
-        this.promptDiv.classList.add('show');
-        this.overlayDiv.classList.add('show');
-        return;
+        this.showPrompt(todo);
       }
     });
 
@@ -50,9 +27,9 @@ class App {
       let button = event.target;
       if(!button.classList.contains('confirm_yes') && !button.classList.contains('confirm_no')) return;
 
+      // needed for deleting actual todo instead of just rendering
+      let todoId = Number(button.closest('.confirm_wrapper').dataset.id);
       if(button.classList.contains('confirm_yes')) {
-        let id = this.promptDiv.dataset.id;
-        let todo = this.todosDiv.querySelector(`li[data-id="${id}"]`);
         this.todosDiv.removeChild(todo);
       }
 
@@ -63,6 +40,24 @@ class App {
       if(!event.target.classList.contains('overlay')) return;
       this.hidePrompt();
     });
+  }
+
+  confirmTemplate(todo) {
+    return `
+      <div class="confirm_wrapper" data-id="${todo.id}">
+        <p>Are you sure you want to delete "${todo.title}"?</p>
+        <div class="actions">
+          <a href="#" class="confirm_yes">Yes</a>
+          <a href="#" class="confirm_no">No</a>
+        </div>
+      </div>
+    `;
+  }
+
+  showPrompt(todo) {
+    this.promptDiv.innerHTML = this.confirmTemplate(todo);
+    this.promptDiv.classList.add('show');
+    this.overlayDiv.classList.add('show');
   }
 
   hidePrompt() {
