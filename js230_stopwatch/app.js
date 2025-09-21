93d3d738fc3d965868bef52e1681a9edd2387e90
William <william.seipp@gmail.com>
2025-09-21 13:10:36 -0700

    feat: implement text toggling on toggle button

--- js230_stopwatch/app.js
+++ js230_stopwatch/app.js
@@ -1,6 +1,16 @@
 class App {
   constructor() {
-    alert('lets party!')
+    this.toggleButton = document.querySelector('.toggle');
+    this.toggleButton.addEventListener('click', this.handleToggle.bind(this));
+  }
+
+  handleToggle(event) {
+    let text = event.target.textContent;
+    if (text === 'Start') {
+      this.toggleButton.textContent = 'Stop';
+    } else {
+      this.toggleButton.textContent = 'Start';
+    }
   }
 }
 
