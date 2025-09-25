cd0bcce325ca7befbd87d9108345d6f9ed32c275
William <william.seipp@gmail.com>
2025-09-25 12:39:18 -0700

    refactor: extract templating logic into designated filter template method

--- js230_cars/app.js
+++ js230_cars/app.js
@@ -39,40 +39,35 @@ class App {
   }
 
   renderFilters() {
-    // this.filtersDiv.innerHTML = filterData.join("");
-    this.filtersDiv.innerHTML = `
-   <label for='make_select'>Make</label>
-   <select id='make_select' name='make'>
-     <option value="">Any</option>
-     <option value="Honda">Honda</option>
-     <option value="Toyota">Toyota</option>
-   </select>
-   <label for='model_select'>Model</label>
-   <select id='model_select' name='model'>
-     <option value="">Any</option>
-     <option value="A4">A4</option>
-     <option value="Camry">Camry</option>
-   </select>
-   <label for='price_select'>Price</label>
-   <select id='price_select' name='price'>
-     <option value="">Any</option>
-     <option value="7000">7000</option>
-     <option value="12000">12000</option>
-   </select>
-   <label for='year_select'>Year</label>
-   <select id='year_select' name='year'>
-     <option value="">Any</option>
-     <option value="2005">2005</option>
-     <option value="2011">2011</option>
-   </select>
-`
+    this.filtersDiv.innerHTML = this.filterTemplate();
   }
 
   filterTemplate(filter) {
     return `
-          <p class="price">Price: $${car.price}</p>
-          <a href="#">Buy</a>
-        </div>
+       <label for='make_select'>Make</label>
+       <select id='make_select' name='make'>
+         <option value="">Any</option>
+         <option value="Honda">Honda</option>
+         <option value="Toyota">Toyota</option>
+       </select>
+       <label for='model_select'>Model</label>
+       <select id='model_select' name='model'>
+         <option value="">Any</option>
+         <option value="A4">A4</option>
+         <option value="Camry">Camry</option>
+       </select>
+       <label for='price_select'>Price</label>
+       <select id='price_select' name='price'>
+         <option value="">Any</option>
+         <option value="7000">7000</option>
+         <option value="12000">12000</option>
+       </select>
+       <label for='year_select'>Year</label>
+       <select id='year_select' name='year'>
+         <option value="">Any</option>
+         <option value="2005">2005</option>
+         <option value="2011">2011</option>
+       </select>
     `;
   }
 }
