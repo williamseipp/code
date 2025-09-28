8126d2a2589d35eae02ffceb8bcef4c68387116a
William <william.seipp@gmail.com>
2025-09-28 16:26:19 -0700

    fix: revert car years back to number type

--- js230_cars/app.js
+++ js230_cars/app.js
@@ -1,11 +1,11 @@
 const cars = [
-  { make: 'Honda', image: 'images/honda-accord-2005.jpg', model: 'Accord', year: '2005', price: 7000 },
-  { make: 'Honda', image: 'images/honda-accord-2008.jpg', model: 'Accord', year: '2008', price: 11000 },
-  { make: 'Toyota', image: 'images/toyota-camry-2009.jpg', model: 'Camry', year: '2009', price: 12500 },
-  { make: 'Toyota', image: 'images/toyota-corrolla-2016.jpg', model: 'Corolla', year: '2016', price: 15000 },
-  { make: 'Suzuki', image: 'images/suzuki-swift-2014.jpg', model: 'Swift', year: '2014', price: 9000 },
-  { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: '2013', price: 25000 },
-  { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: '2013', price: 26000 },
+  { make: 'Honda', image: 'images/honda-accord-2005.jpg', model: 'Accord', year: 2005, price: 7000 },
+  { make: 'Honda', image: 'images/honda-accord-2008.jpg', model: 'Accord', year: 2008, price: 11000 },
+  { make: 'Toyota', image: 'images/toyota-camry-2009.jpg', model: 'Camry', year: 2009, price: 12500 },
+  { make: 'Toyota', image: 'images/toyota-corrolla-2016.jpg', model: 'Corolla', year: 2016, price: 15000 },
+  { make: 'Suzuki', image: 'images/suzuki-swift-2014.jpg', model: 'Swift', year: 2014, price: 9000 },
+  { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: 2013, price: 25000 },
+  { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: 2013, price: 26000 },
 ];
 
 class App {
