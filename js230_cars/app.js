9c27ab82d2ddf2e3ffd7f2001b36c0d878902b3b
William <william.seipp@gmail.com>
2025-09-28 16:18:18 -0700

    feat: implement filtering of cars

--- js230_cars/app.js
+++ js230_cars/app.js
@@ -1,11 +1,11 @@
 const cars = [
-  { make: 'Honda', image: 'images/honda-accord-2005.jpg', model: 'Accord', year: 2005, price: 7000 },
-  { make: 'Honda', image: 'images/honda-accord-2008.jpg', model: 'Accord', year: 2008, price: 11000 },
-  { make: 'Toyota', image: 'images/toyota-camry-2009.jpg', model: 'Camry', year: 2009, price: 12500 },
-  { make: 'Toyota', image: 'images/toyota-corrolla-2016.jpg', model: 'Corolla', year: 2016, price: 15000 },
-  { make: 'Suzuki', image: 'images/suzuki-swift-2014.jpg', model: 'Swift', year: 2014, price: 9000 },
-  { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: 2013, price: 25000 },
-  { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: 2013, price: 26000 },
+  { make: 'Honda', image: 'images/honda-accord-2005.jpg', model: 'Accord', year: '2005', price: 7000 },
+  { make: 'Honda', image: 'images/honda-accord-2008.jpg', model: 'Accord', year: '2008', price: 11000 },
+  { make: 'Toyota', image: 'images/toyota-camry-2009.jpg', model: 'Camry', year: '2009', price: 12500 },
+  { make: 'Toyota', image: 'images/toyota-corrolla-2016.jpg', model: 'Corolla', year: '2016', price: 15000 },
+  { make: 'Suzuki', image: 'images/suzuki-swift-2014.jpg', model: 'Swift', year: '2014', price: 9000 },
+  { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: '2013', price: 25000 },
+  { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: '2013', price: 26000 },
 ];
 
 class App {
@@ -83,8 +83,8 @@ class App {
 
   handleFilterClick(event) {
     this.setFilterState();
-
-    alert(JSON.stringify(this.filterState, null, 2));
+    this.updateFilteredCars();
+    this.renderCars();
   }
 
   setFilterState() {
@@ -92,6 +92,22 @@ class App {
     this.filterState.model = this.filtersDiv.querySelector('select[name="model"]').value,
     this.filterState.price = this.filtersDiv.querySelector('select[name="price"]').value,
     this.filterState.year = this.filtersDiv.querySelector('select[name="year"]').value
+  }
+
+
+  updateFilteredCars() {
+    // Find which filter fields are set (not empty strings)
+    const activeFilterKeys = Object.keys(this.filterState).filter(
+      key => this.filterState[key] !== ''
+    );
+
+    // // Go through all cars and return those that match ALL active filter fields
+    this.filteredCars = this.allCars.filter(car =>
+      // Check if every active filter key matches between car and filterState
+      activeFilterKeys.every(
+        key => car[key] === this.filterState[key]
+      )
+    );
   }
 }
 
